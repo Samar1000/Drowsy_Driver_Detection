9a6f7ca528094c1cfdfa10dca7c6b4cb23f8a108
Samar1000 <136191152+Samar1000@users.noreply.github.com>
2023-07-16 08:12:36 +0300

    Add files via upload

---
+++ Atmega32/include/HAL/sensor/sensor_configration.h
@@ -0,0 +1,16 @@
+/*
+ * sensor_configration.h
+ *
+ *  Created on: Mar 2, 2023
+ *      Author: hp
+ */
+
+#ifndef INCLUDE_HAL_SENSOR_SENSOR_CONFIGRATION_H_
+#define INCLUDE_HAL_SENSOR_SENSOR_CONFIGRATION_H_
+
+#define PARKING_DEPTH		16	    /* in Centimeters */
+#define ITERATIONS			250
+#define PARKING_DISTANCE	40		/* in Centimeters */
+
+
+#endif /* INCLUDE_HAL_SENSOR_SENSOR_CONFIGRATION_H_ */
